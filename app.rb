b3a5f9f8686a92f31e97026d03aa2fcff54fb692
Rick Walker <walkrick@yahoo.com>
2014-07-02 16:07:42 -0600

    first commit

--- app.rb
+++ app.rb
@@ -13,6 +13,22 @@ class App < Sinatra::Application
   end
 
   get "/" do
-    "Hello, world"
+    erb :home
   end
+
+  get "/registration" do
+    erb :registration
+  end
+
+  post "/registration" do
+    flash[:notice] = "Thank you for registering"
+    @user_database.insert({username: params[:name], password: params[:password]})
+    redirect "/"
+  end
+
+  # post "/" do
+  #   flash[:login] = "Welcome," + params[:name]
+  #   redirect "/"
+  #
+  # end
 end
